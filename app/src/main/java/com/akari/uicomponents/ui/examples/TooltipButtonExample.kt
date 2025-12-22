db940a1b8e99a1cbf2567089832aef8e61d296ec
Akari <venerosandres10@gmail.com>
2025-12-21 19:22:54 -0600

    delete AkariTooltipButtons and added AkariTooltip for create tooltips more easely

--- app/src/main/java/com/akari/uicomponents/ui/examples/TooltipButtonExample.kt
+++ app/src/main/java/com/akari/uicomponents/ui/examples/TooltipButtonExample.kt
@@ -1,31 +1,49 @@
 package com.akari.uicomponents.ui.examples
 
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.dp
-import com.akari.uicomponents.buttons.AkariButtonVariant
-import com.akari.uicomponents.buttons.config.AkariButtonConfig
-import com.akari.uicomponents.buttons.tooltipButtons.AkariTooltipButton
+import com.akari.uicomponents.tooltip.AkariTooltip
+import com.akari.uicomponents.tooltip.AkariTooltipConfig
+import com.akari.uicomponents.tooltip.AkariTooltipStyle
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TooltipButtonExample(){
     val shapes = MaterialTheme.shapes
     val colors = MaterialTheme.colorScheme
-    AkariTooltipButton(
-        variant = AkariButtonVariant.Filled,
-        onClick = {},
-        buttonConfig = AkariButtonConfig {
-            shape = shapes.extraSmall
-            border = BorderStroke(width = 3.dp, color = colors.tertiaryContainer)
-        },
-        tooltipContent = {
-            Text(text = "Tooltip Example")
+    Column() {
+        AkariTooltip(
+            text = "Tooltip Example",
+            tooltipConfig = AkariTooltipConfig(
+                persistent = true
+            ),
+            style = AkariTooltipStyle.PlainTooltip(
+                containerColor = colors.primary,
+                contentColor = colors.onPrimary
+            )
+        ) {
+            Button(
+                onClick = {},
+                shape = shapes.extraSmall
+            ) {
+                Text(text = "Hello World")
+            }
         }
-    ){
-        Text(text = "Hello World")
+        AkariTooltip(
+            tooltipContent = {
+                Text(text = "Tooltip Example")
+            }
+        ) {
+            Button(
+                onClick = {},
+                shape = shapes.extraSmall
+            ) {
+                Text(text = "Hello World")
+            }
+        }
     }
 }
