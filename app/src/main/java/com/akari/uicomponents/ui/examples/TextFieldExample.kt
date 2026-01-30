ba115caac84d336c02bac4afa64a93bcd7932598
Andres Veneros <dev@veneros.dev>
2026-01-29 20:54:58 -0600

    fix position trailicon in textfield

--- app/src/main/java/com/akari/uicomponents/ui/examples/TextFieldExample.kt
+++ app/src/main/java/com/akari/uicomponents/ui/examples/TextFieldExample.kt
@@ -1,5 +1,6 @@
 package com.akari.uicomponents.ui.examples
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.TextFieldState
@@ -13,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
@@ -46,6 +48,15 @@ fun TextFieldExample() {
                     contentDescription = "Search",
                 )
             }
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search",
+                )
+            }
+            prefix = { Text("Prefix") }
+            suffix = { Text("Suffix") }
+            supportingText = { Text("Supporting text") }
         }
         style {
             shape = shapes.extraSmall
@@ -64,6 +75,7 @@ fun TextFieldExample() {
     }
 
     AkariTextField(
+        modifier = Modifier.fillMaxWidth(),
         value = value,
         onValueChange = { value = it },
         config = config,
